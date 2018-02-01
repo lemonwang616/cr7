de3fd63f4d41ef94b7658000f1d40db1a2a3644a
unknown <lemon.wang.poppy@gmail.com>
2018-02-01 16:59:12 +0800

    1015xx

--- main.cpp
+++ main.cpp
@@ -1,52 +1,60 @@
 /*
-输入样例：
-3 4 -5 2 6 1 -2 0
-输出样例：
-12 3 -10 1 6 0
+德 Morality  才 Talent 
+N 考生总数   L录取最低分数线  H 优先录取线 
+第一类  德才全尽  德 才 全大于H   按德才总分高到低排序
+第二类   德胜才    德>=H  才<H  按德才总分高到低排序
+第三类   德才均低于H  德>=才  按总分排序
+第四类  德、才达到最低线L  按总分排序 
 */
 #include <string>
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <stdio.h>
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
 
+bool isPrime(int num)
+{
+	if(num==0||num==1)
+	{
+		return false;
+	}
+	int i;
+	for(i=2;i*i<=num;i++)
+	{
+		if(num%i==0)
+		return false;
+	}
+	return true;
+}
+
+struct Info
+{
+	string id;
+	int Morality;
+	int Talent;
+	int total;
+};
+
 int main()
 {
-	vector<int> vec;
-	char ch;
-	int temp;
-	while(cin>>temp)
-	{
-		vec.push_back(temp);
-		ch=getchar();
-		if(ch=='\n')
-			break;
-		if(ch==' ')
-			continue;
-	}
-	int length=vec.size();
+	int N,L,H;
+	cin>>N;//people number
+	cin>>H;//the minimum adimission line
+	cin>>H;//the excellent line
+	Info Infom[N];
 	int i,j;
-	vector<int> Deriva;
-	for(i=0;i<length/2;i++)
+	for(i=0;i<N;i++)
 	{
-		if(vec[2*i+1]==0)
-		{
-			//cout<<"i="<<2*i+1<<endl;
-			continue;
-		}
-		int temp1=vec[2*i]*vec[2*i+1];
-		Deriva.push_back(temp1);
-		int temp2=vec[2*i+1]-1;
-		Deriva.push_back(temp2);		
+		cin>>Infom[i].id;
+		cin>>Infom[i].Morality;
+		cin>>Infom[i].Talent;
+		Infom[i].total=Infom[i].Morality+Infom[i].Talent;
 	}
-	int len=Deriva.size();
-	for(i=0;i<len-1;i++)
-		cout<<Deriva[i]<<" ";
-	cout<<Deriva[len-1];
 	return 0;	
 }
 
