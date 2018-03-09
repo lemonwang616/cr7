6b54dd205ef0228c8c9f81b0cd93584fede0879c
unknown <lemon.wang.poppy@gmail.com>
2018-03-09 11:45:47 +0800

    1003 NAC

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
+//
+//  main.cpp
+//  0208
+//
+//  Created by lemon_wang on 18/2/8.
+//  Copyright ? 2018年 lemon_wang. All rights reserved.
+//
+
 #include <string>
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <stdio.h>
-#include   <map>
+#include <map>
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
 using std::map;
-
+#define INF 9999999
 bool isPrime(int num)
 {
     if(num==0||num==1)
@@ -25,234 +33,95 @@ bool isPrime(int num)
     }
     return true;
 }
-long long GCD(long long a,long long b)
+bool iicompare(int a,int b)
 {
-    long long m=a%b;
-    while(m)
-    {
-        a=b;
-        b=m;
-        m=a%b;
-    }
-  //  if(b<0)
-        //b=-b;
-    return b;
+	return a<b;
 }
-bool isequal(int * conver,int * temp,int N)
+typedef struct rescueteam
 {
-    bool flag=true;
-    int i;
-    for(i=0;i<N;i++)
-    {
-        if(conver[i]!=temp[i])
-        {
-            flag=false;
-            break;
-        }
-    }
-    return flag;
-}
-    
-bool InsertionSort(int * origin,int * conver,int N)
-{
-    int count=0;
-    bool flag=false;
-    //cout<<"INsertion  "<<N<<endl;
-    int i,j,k;
-    int temp;
-    for(i=1;i<N;i++)
-    {
-        //cout<<i<<endl;
-        for(j=0;j<i;j++)
-        {
-            if(origin[i]<origin[j])
-            {
-                temp=origin[i];
-                k=i;
-                while(k!=j)
-                {
-                    //cout<<k<<" "<<j<<endl;
-                    origin[k]=origin[k-1];
-                    k--;
-                    //cout<<k<<" "<<origin[k]<<k+1<<origin[k+1]<<endl;
-                    
-                }
-                origin[j]=temp;
-            }
-        }
-        flag=isequal(conver,origin,N);
-        if(count==1)
-        {
-            cout<<"Insertion Sort"<<endl;
-            for(i=0;i<N-1;i++)
-                printf("%d ",origin[i]);
-           printf("%d",origin[N-1]);
-            return flag;
-        }
-        if(flag==true)
-        {
-            count++;
-        }
-        
-    }
-    return flag;
-}
-void merge(int * array,int low,int mid,int high) // [low,mid)  [mid,high)
-{
-    int temp[high-low];
-    int i=low;
-    int j=mid;
-    int k=0;
-    while(i<mid&&j<high)
-    {
-        if(array[i]<=array[j])
-        {
-            temp[k++]=array[i++];
-        }
-        else
-        {
-            temp[k++]=array[j++];
-        }
-    
-    }
-    while(i<mid)
-    {
-        temp[k++]=array[i++];
-    }
-    while(j<high)
-    {
-        temp[k++]=array[j++];
-    }
-    for(i=low,k=0;i<high;i++,k++)
-    {
-        array[i]=temp[k];
-    }
-}
-
-bool MergeSort(int * origin,int * conver,int N)
-{
-    bool flag=false;
-    int count=0;
-    int i,j,k;
-    for(i=1;i<N;i=i*2)
-    {
-        for(j=0;j<N;j=j+2*i)
-        {
-            if(j+2*i<=N)
-            {
-                merge(origin,j,j+i,j+2*i);
-                //cout<<"merge(j,j+i,j+2*i)"<<endl;
-            }
-            else
-            {
-                if(j+i<=N)
-                {
-                    merge(origin,j,j+i,N);
-                    //cout<<"merge(j,j+i,N)"<<endl;
-                
-                }
-            }
-        }
-        flag=isequal(conver,origin,N);
-        if(count==1)
-         {
-         cout<<"Merge Sort"<<endl;
-         for(i=0;i<N-1;i++)
-         printf("%d ",origin[i]);
-         printf("%d",origin[N-1]);
-         return flag;
-         
-         }
-        if(flag==true)
-        {
-            count++;
-        }
-       
-    
-        
-    }
-    
-    return flag;
-
-}
-void mergeSort(int * origin,int * conver,int N)
-{
-    //bool flag=false;
-    //int count=0;
-    int i,j,k;
-    for(i=1;i<N;i=i*2)
-    {
-        for(j=0;j<N;j=j+2*i)
-        {
-            if(j+2*i<=N)
-            {
-                merge(origin,j,j+i,j+2*i);
-                //cout<<"merge(j,j+i,j+2*i)"<<endl;
-            }
-            else
-            {
-                //if(j+i<=N)
-                //{
-                    merge(origin,j,j+i,N);
-                    //cout<<"merge(j,j+i,N)"<<endl;
-                    
-                //}
-            }
-        }
-     
-        for(k=0;k<N;k++)
-            printf(" %d",origin[k]);
-        printf("\n");
-        
-        
-    }
-    
-   
-    
-}
-void compare(int * origin,int * conver,int N)
-{
-    bool flag=false;
-    int temp[N];
-    int i;
-    for(i=0;i<N;i++)
-    {
-        temp[i]=origin[i];
-    }
-    flag=InsertionSort(origin,conver,N);
-    if(flag==true)
-        return;
-    else
-    {
-    MergeSort(temp,conver,N);
-        //cout<<"213"<<endl;
-        return;
-    }
-}
+	int count;
+	int resnum;
+}resut;
 int main()
 {
-    //freopen("a.txt","r",stdin);
-    int N;
-    cin>>N;
-    int origin[N];
-    int conver[N];
-   /* int InsertArray[N];
-   int MergeArray[N];*/
-    int i,j,k;
-   for(i=0;i<N;i++)
-        scanf("%d",&origin[i]);
-    for(i=0;i<N;i++)
-        scanf("%d",&conver[i]);
-    /* for(i=0;i<N;i++)
-        printf("%d",origin[i]);
-    printf("\n");
-    for(i=0;i<N;i++)
-        printf("%d",conver[i]);
-    printf("\n");*/
-    mergeSort(origin,conver,N);
-    //compare(origin,conver,N);
-  // MergeSort(origin,conver,N);
-   
-   //InsertionSort(origin,conver,N);
-    return 0;
+	
+	int N,M,C1,C2;
+	cin>>N>>M>>C1>>C2;
+	int res[N];
+	int shortime[N];//记录C1到每个点的最短路径的次数 
+
+	int flag[N]={0};
+	int resu[N][N];//存储救援队的个数 
+	int i,j,k;
+	for(i=0;i<N;i++)
+		scanf("%d",&res[i]);
+	int rode[N][N];
+	for(i=0;i<N;i++)
+		for(j=0;j<N;j++)
+		{
+			rode[i][j]=INF;
+			resu[i][j]=0;
+		}
+	int start,end,temp;
+	for(i=0;i<M;i++)
+	{
+		scanf("%d %d %d",&start,&end,&temp);
+		rode[start][end]=temp;
+		rode[end][start]=temp;
+		resu[start][end]=res[start]+res[end];
+		resu[end][start]=res[start]+res[end];
+		if(start==C1)
+		{
+			shortime[end]=1;
+		}
+	}
+
+
+	flag[C1]=1;
+	int minlen;
+	int node;//record current min node
+	
+	for(k=0;k<N-1;k++)
+	{
+		minlen=INF;
+		for(i=0;i<N;i++)
+		{
+			if(flag[i]==0&&(rode[C1][i]<minlen))
+			{
+					minlen=rode[C1][i];
+					node=i;
+			}
+		}
+		flag[node]=1;
+		//cout<<node<<endl; 
+		for(i=0;i<N;i++)
+		{
+			if(flag[i]==0&&((rode[C1][node]+rode[node][i])<=rode[C1][i]))
+			{
+				
+				if((rode[C1][node]+rode[node][i])<rode[C1][i])
+				{
+					shortime[i]=shortime[node];	
+					resu[C1][i]=resu[C1][node]+res[i];
+				}
+				else  if((rode[C1][node]+rode[node][i])==rode[C1][i])
+				{
+					shortime[i]=shortime[i]+shortime[node];
+					if((resu[C1][node]+res[i])>resu[C1][i])
+					{
+						resu[C1][i]=resu[C1][node]+res[i];
+					}
+				}
+				rode[C1][i]=rode[C1][node]+rode[node][i];
+				
+			}
+			//每次更新路径时，存入最大救援数量和最短距离 
+		}
+	
+	}
+//	cout<<rode[C1][C2]<<" "<<shortime[C2]<<" "<<resu[C1][C2];
+	cout<<shortime[C2]<<" "<<resu[C1][C2];
+	return 0;
 }
+
+
