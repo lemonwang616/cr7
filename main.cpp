f020e451ae9c41bc7d56ff3a4d7eca34a1e115e7
unknown <lemon.wang.poppy@gmail.com>
2018-03-12 20:50:01 +0800

    vec method

--- main.cpp
+++ main.cpp
@@ -7,12 +7,14 @@
 //
 
 #include <string>
+#include <cstring> 
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <stdio.h>
 #include   <map>
 #include <queue>
+#include <math.h>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -60,75 +62,49 @@ void DFS(Tree ttre,int level)
 		DFS(tree[ttre.child[i]],level+1);
 	}
 }
+struct Check_intable
+{
+
+  char id[20];
+  char sign_in_time[10];
+  char sign_out_time[10];
+};
+bool SmallerSort(Check_intable a,Check_intable b)
+{
+	if(strcmp(a.sign_in_time,b.sign_in_time)>0)
+		return false;
+	else
+		return true;
+	//return a.sign_in_time>b.sign_in_time;
+}
+bool GreaterSort(Check_intable a,Check_intable b)
+{
+	if(strcmp(a.sign_out_time,b.sign_out_time)<0)
+		return false;
+	else
+		return true;
+	//return a.sign_out_time>b.sign_out_time;
+}
 int main()
 {
-    int N,M;//the num of nodes   M the num of leafnodes
-    cin>>N>>M;
-    int i,j,k;
-   // Tree tree[101];//vector
-    //vector<Tree>
-    for(i=1;i<=N;i++)
-    {
-        Tree temp;
-        temp.node=i;
-        tree[i]=temp;
-    }
-    //vector<Tree> trees;
-    int node;
-    int num;
-    int temp;
-    for(k=0;k<M;k++)
-    {
-        cin>>node>>num;
-        tree[node].num=num;
-        tree[node].child=new int[num];
-        for(i=0;i<num;i++)
-        {
-            scanf("%d",&temp);
-            tree[node].child[i]=temp;
-        }
-    }
-    DFS(tree[1],1);
-    temp=N-M;
-    printf("%d",records[1]);
-    num=records[1];
-    for(i=2;num<temp;i++)
-    {
-    	printf(" %d",records[i]);
-    	num=num+records[i];
-	}
-    /*queue<int> currentlevel;//  current level
-        bool  firstflag=true;
-    currentlevel.push(1);
-    do{
-        int count=0;//  record this level's non leaf node
-        queue<int> nextlevel;//    next level
-        while(currentlevel.empty()==0)
-        {
-            int cnode=currentlevel.front();
-            currentlevel.pop();
-            if(tree[cnode].num==0)
-            {
-                count++;
-            }
-            else{
-                for(i=0;i<tree[cnode].num;i++)
-                {
-                    nextlevel.push(tree[cnode].child[i]);
-                }
-            }
-        }
-        if(firstflag==true)
-        {
-            printf("%d",count);
-            firstflag=false;
-        }
-        else
-        {
-            printf(" %d",count);
-        }
-        currentlevel=nextlevel;
-    }while(currentlevel.size()!=0);*/
-    
+ int M;
+ cin>>M;
+ vector<Check_intable> vec;
+ int i,j;
+ Check_intable temptable;
+ for(i=0;i<M;i++)
+ {
+ 	scanf("%s %s %s",&temptable.id,&temptable.sign_in_time,&temptable.sign_out_time);
+ //	cout<<i<<endl;
+	//scanf("%s",&temptable.sign_in_time);
+	//scanf("%s",&temptable.sign_out_time);
+	vec.push_back(temptable);
+ }
+	sort(vec.begin(),vec.end(),SmallerSort);
+	cout<<vec[0].id<<" ";
+	//cout<<vec[0].id<<" "<<vec[0].sign_in_time<<endl;
+	sort(vec.begin(),vec.end(),GreaterSort);
+	//cout<<vec[0].id
+	cout<<vec[0].id<<" "<<vec[0].sign_out_time<<endl;
     return 0;
 }
